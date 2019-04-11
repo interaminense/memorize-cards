ff1944969f937b75ea2121e3ab22771fbf990fcb
Adriano Interaminense <adriano.interaminense@gmail.com>
2019-04-11 00:31:32 -0300

    Updates

---
+++ precache-manifest.1279b5fe41e8d5b048cf510554bc90b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c42971e0d4de772ed3",
+    "url": "/memorize-cards/static/js/runtime~main.9da272da.js"
+  },
+  {
+    "revision": "3ad76f93ccf13ec3fdd9",
+    "url": "/memorize-cards/static/js/main.d0023fa4.chunk.js"
+  },
+  {
+    "revision": "87c4f02bee46e42c2877",
+    "url": "/memorize-cards/static/js/2.c4ad3bc2.chunk.js"
+  },
+  {
+    "revision": "3ad76f93ccf13ec3fdd9",
+    "url": "/memorize-cards/static/css/main.b006ae5a.chunk.css"
+  },
+  {
+    "revision": "cc416ba2829094a19e87576a8eccc3ca",
+    "url": "/memorize-cards/index.html"
+  }
+];
