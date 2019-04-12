4ddf6dddfac3d76523cf722d018d27402cadba63
Adriano Interaminense <adriano.interaminense@gmail.com>
2019-04-11 23:46:54 -0300

    Use Slick component to render Slide Carousel

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -1,67 +1,22 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Card from './Card';
+import Slider from "react-slick";
 
-const CardItems = ({items, activeItem}) => (
-    <ul className="card-group-items">
-        {items.map((item, index) => {
-            const active = parseInt(activeItem) === index;
-            const classes = `card-group-item ${active ? 'active' : 'initial'}`;
-
-            return (
-                <li
-                    className={classes}
-                    key={index}
-                >
-                    <Card card={item} />
-                </li>
-            )
-        })}
-    </ul>
-);
-
-const Pagination = props => {
-    const {items, onChange} = props;
-
-    const handleClickItem = ({target}) => {
-        const {index} = target.dataset;
-
-        onChange && onChange(index);
-    }
-
-    return (
-        <div className="card-pagination">
-            {items.map((item, index) => (
-                <button
-                    data-index={index}
-                    onClick={handleClickItem}
-                    key={index}>
-                </button>
-            ))}
-        </div>
-    )
+const settings = {
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 300
 };
 
-const CardGroup = ({items}) => {
-    const [state, setState] = useState({activeItem: 0});
-
-    const handleChangeItem = index => {
-        setState({activeItem: index});
-    }
-
-    const {activeItem} = state;
-
-    return (
-        <li className="card-group">
-            <CardItems
-                activeItem={activeItem}
-                items={items}
-            />
-            <Pagination
-                onChange={handleChangeItem}
-                items={items}
-            />
-        </li>
-    );
-}
+const CardGroup = ({items}) => (
+    <li className="cards-group-item">
+        <Slider {...settings}>
+            {items.map((item, index) => (
+                <Card card={item} key={index} />
+            ))}
+        </Slider>
+    </li>
+);
 
 export default CardGroup;
