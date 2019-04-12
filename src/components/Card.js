93c9ed75f46548ca7eb7136a048604afbe03a5aa
alexandre araujo <41647261+oliveiraaraujo@users.noreply.github.com>
2019-04-12 10:59:33 -0300

    sf bchild

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-const Card = ({card}) => {
-    const {title, subtitle, description} = card;
-
-    return (
-        <div className="card">
-            <h3 className="card-title">{title}</h3>
-            <small className="card-subtitle">{subtitle}</small>
-            <h1 className="card-description">{description}</h1>
-        </div>
-    );
-}
+const Card = ({card: {title, subtitle, description}}) => (
+    <div className="card">
+        <h3 className="card-title">{title}</h3>
+        <small className="card-subtitle">{subtitle}</small>
+        <h1 className="card-description">{description}</h1>
+    </div>
+);
 
 export default Card;
