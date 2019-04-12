5a7483f2ec0736e4689cb486d11031a8bcdb4b23
Adriano Interaminense <adriano.interaminense@gmail.com>
2019-04-11 23:47:24 -0300

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WithCards from './hocs/Cards';
+import Header from './components/Header';
 
 import './style/main.scss';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header title="Memorize App" />
         <WithCards />
       </div>
     );
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,12 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import CardGroup from './CardGroup';
 
-const Cards = props => {
-    const {cards} = props;
-
-    return (
-        <Fragment>
-            <h1 className="title">Memorize!</h1>
-
-            <ul className="cards-group">
-                {cards.map(({items}, index) => (
-                    <CardGroup items={items} key={index} />
-                ))}
-            </ul>
-        </Fragment>
-    );
-}
+const Cards = ({cards}) => (
+    <ul className="cards-group">
+        {cards.map(({items}, index) => (
+            <CardGroup items={items} key={index} />
+        ))}
+    </ul>
+);
 
 export default Cards;
